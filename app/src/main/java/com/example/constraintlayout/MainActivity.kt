b0e4b739381d5446a3637fd5eea6e86558a10047
name <fanxiaojuan@zork.com.cn>
2020-02-27 15:29:29 +0800

    约束布局

--- app/src/main/java/com/example/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/example/constraintlayout/MainActivity.kt
@@ -8,6 +8,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_constraint_layout2)
+        setContentView(R.layout.activity_case)
+
+        //案例
+        //需求:要求产品类型紧跟产品名称后面,当产品名称过长使用“…”省略,不会影响产品类型展示,如下正常效果图. 可使用ConstraintLayout实现,当文字过长时也可以正常展示(如下文字过长效果图)
     }
 }
