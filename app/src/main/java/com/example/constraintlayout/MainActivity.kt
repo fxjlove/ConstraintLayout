13f86fce96ac8c7af86ae6fdfb4881d68980fceb
name <fanxiaojuan@zork.com.cn>
2020-02-27 14:31:23 +0800

    ConstraintLayout

---
+++ app/src/main/java/com/example/constraintlayout/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//https://blog.csdn.net/zping0808/article/details/83064568
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout_placeholder)
+    }
+}
